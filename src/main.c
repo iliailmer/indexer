ce5ccd866b324efbccce062bf6dcb1b9724161d4
Ilia <i.ilmer@yahoo.com>
2025-02-28 11:45:26 -0600

    add rb-trees

--- src/main.c
+++ src/main.c
@@ -1,44 +1,185 @@
-// TODO: add hash table in get info, support for cachinhg and specific file info
-// TODO: min-heap: Enhance the list command to quickly find the N largest files.
-
+// TODO: Add Search
+// TODO: Add checks for last indexed paths
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <time.h>
+#define RED 1
+#define BLACK 0
 
 typedef struct {
   char *cmd;
   void (*funct)();
-} CmdTable; // basically a hash table / dictionary of coimmands
+} CmdTable; // basically a table of coimmands, currently O(n)
+
+typedef struct FileTreeNode {
+  char filename[256];
+  long mem_usage;
+  time_t last_modified;
+  size_t size;
+  int color;
+  struct FileTreeNode *left;
+  struct FileTreeNode *right;
+  struct FileTreeNode *parent;
+} FileTreeNode; // Red-Black tree node for file system info storage
+
+FileTreeNode *root = NULL;
+
+// RB Tree functions
+
+void rotate_left(FileTreeNode **root, FileTreeNode *x) {
+  FileTreeNode *y = x->right;
+  x->right = y->left;
+  if (y->left != NULL) {
+    y->left->parent = x;
+  }
+  y->parent = x->parent;
+  if (x->parent == NULL) {
+    *root = y;
+  } else if (x == x->parent->left) {
+    x->parent->left = y;
+  } else {
+    x->parent->right = y;
+  }
+  y->left = x;
+  x->parent = y;
+}
+
+void rotate_right(FileTreeNode **root, FileTreeNode *x) {
+  FileTreeNode *y = x->left;
+  x->left = y->right;
+  if (y->right != NULL) {
+    y->right->parent = x;
+  }
+  y->parent = x->parent;
+  if (x->parent == NULL) {
+    *root = y;
+  } else if (x == x->parent->right) {
+    x->parent->right = y;
+  } else {
+    x->parent->left = y;
+  }
+  y->right = x;
+  x->parent = y;
+}
+
+void insert_fixup(FileTreeNode **root, FileTreeNode *z) {
+  while (z->parent != NULL && z->parent->color == RED) {
+    if (z->parent == z->parent->parent->left) {
+      FileTreeNode *y = z->parent->parent->right;
+      if (y != NULL && y->color == RED) { // Case 1: Uncle is RED
+        z->parent->color = BLACK;
+        y->color = BLACK;
+        z->parent->parent->color = RED;
+        z = z->parent->parent;
+      } else {
+        if (z == z->parent->right) { // Case 2: z is right child
+          z = z->parent;
+          rotate_left(root, z);
+        }
+        // Case 3: z is left child
+        z->parent->color = BLACK;
+        z->parent->parent->color = RED;
+        rotate_right(root, z->parent->parent);
+      }
+    } else { // Mirror cases for right subtree
+      FileTreeNode *y = z->parent->parent->left;
+      if (y != NULL && y->color == RED) {
+        z->parent->color = BLACK;
+        y->color = BLACK;
+        z->parent->parent->color = RED;
+        z = z->parent->parent;
+      } else {
+        if (z == z->parent->left) {
+          z = z->parent;
+          rotate_right(root, z);
+        }
+        z->parent->color = BLACK;
+        z->parent->parent->color = RED;
+        rotate_left(root, z->parent->parent);
+      }
+    }
+  }
+  (*root)->color = BLACK; // Ensure root is always BLACK
+}
+
+// Insert a new file into the Red-Black Tree
+void insert_file(FileTreeNode **root, const char *filename, long size,
+                 time_t modified_time) {
+  FileTreeNode *z = (FileTreeNode *)malloc(sizeof(FileTreeNode));
+  if (!z)
+    return;
+
+  strcpy(z->filename, filename);
+  z->size = size;
+  z->last_modified = modified_time;
+  z->color = RED;
+  z->left = z->right = z->parent = NULL;
+
+  FileTreeNode *y = NULL;
+  FileTreeNode *x = *root;
+
+  while (x != NULL) { // Standard BST insertion
+    y = x;
+    if (strcmp(filename, x->filename) < 0) // Sort by filename
+      x = x->left;
+    else
+      x = x->right;
+  }
+
+  z->parent = y;
+  if (y == NULL)
+    *root = z;
+  else if (strcmp(filename, y->filename) < 0)
+    y->left = z;
+  else
+    y->right = z;
+
+  insert_fixup(root, z); // Fix violations
+}
 
 DIR *open_dir(char *path) {
   printf("%s\b", path);
   DIR *dir = opendir(path);
   if (dir == NULL) {
     perror("opendir failed");
-    // exit(1);
     return NULL;
   }
   return dir;
 }
 
+void print_files(FileTreeNode *node) {
+  if (node == NULL)
+    return;
+  print_files(node->left);
+  printf("%-20s ", node->filename);
+  print_files(node->right);
+}
+
 void list_files(char *path) {
   // NOTE: lists files in the directory provided
   DIR *dir = open_dir(path);
+  struct dirent *_dir;
+  struct stat file_stat;
+  char fullpath[1024];
   if (dir == NULL) {
     return;
   }
-  struct dirent *_dir;
   while ((_dir = readdir(dir)) != NULL) {
-    printf("%s\n", _dir->d_name);
+    snprintf(fullpath, sizeof(fullpath), "%s/%s", path, _dir->d_name);
+    if (stat(fullpath, &file_stat) == 0) {
+      insert_file(&root, _dir->d_name, file_stat.st_size, file_stat.st_mtime);
+    }
   }
+  print_files(root);
   closedir(dir);
 }
 
 void get_input(char *cmd, size_t bufsize) {
   // NOTE: Receives an input from terminal / user
+  printf("\n>  ");
   fgets(cmd, bufsize, stdin);
   cmd[strcspn(cmd, "\n")] = '\0';
 }
@@ -50,13 +191,19 @@ void exit_program(void) {
 
 void get_info(char *path) {
   DIR *dir = open_dir(path);
-  printf("Permissions  Size (bytes)  Last Modified        Name\n");
-  printf("------------------------------------------------------\n");
   struct dirent *_dir;
   struct stat file_stat;
   char fullpath[1024];
+  if (dir == NULL) {
+    return;
+  }
+  printf("Permissions  Size (bytes)  Last Modified        Name\n");
+  printf("------------------------------------------------------\n");
   while ((_dir = readdir(dir)) != NULL) {
     snprintf(fullpath, sizeof(fullpath), "%s/%s", path, _dir->d_name);
+    if (stat(fullpath, &file_stat) == 0) {
+      insert_file(&root, _dir->d_name, file_stat.st_size, file_stat.st_mtime);
+    }
     if (stat(fullpath, &file_stat) == 0) {
       char timebuf[20];
       strftime(timebuf, sizeof(timebuf), "%Y-%m-%d %H:%M",
